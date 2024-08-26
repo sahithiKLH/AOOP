f7e48381948857eac3714a8ffeecf575865f4f22
sahithiKLH <2320090074@klh.edu.in>
2024-08-26 08:57:19 +0530

    Add files via upload

---
+++ 1A/src/Single/Logger.java
@@ -0,0 +1,21 @@
+package Single;
+
+public class Logger {
+    
+    private static Logger single_instance = null;
+    private Logger() {}
+    public static Logger getInstance() {
+        
+                if (single_instance == null) {
+                    single_instance = new Logger();
+               
+            }
+     
+        return single_instance;
+    }
+
+  
+    public void log(String message) {
+        System.out.println("Log message: " + message);
+    }
+}
