52b91c85c44b84bc65a4f1f47336af37d88db645
sahithiKLH <2320090074@klh.edu.in>
2024-08-26 09:00:41 +0530

    Add files via upload

---
+++ 4B/src/LogCommand.java
@@ -0,0 +1,12 @@
+public class LogCommand implements Command {
+    private LogHandler handler;
+
+    public LogCommand(LogHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void execute(String message, LogLevel level) {
+        handler.handle(message, level);
+    }
+}
